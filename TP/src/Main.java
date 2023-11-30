d26e25f561f59709fc1b110bf4f0e0035f4b4016
Galoniax <axelian2504@gmail.com>
2023-11-30 15:56:15 -0300

    FEAT: Create 10 Equipos to class Main.

--- TP/src/Main.java
+++ TP/src/Main.java
@@ -21,5 +21,19 @@ public class Main {
 
         EtapaMundial FaseFinal = new EtapaMundial("Final");
 
+        //Equipos
+        Equipo Argentina = new Equipo("Argentina");
+        Equipo Uruguay = new Equipo("Uruguay");
+        Equipo Paraguay = new Equipo("Paraguay");
+        Equipo Chile = new Equipo("Chile");
+        Equipo Bolivia = new Equipo("Bolivia");
+
+        Equipo Brasil = new Equipo("Brasil");
+        Equipo Peru = new Equipo("Perú");
+        Equipo Colombia = new Equipo("Colombia");
+        Equipo Ecuador = new Equipo("Ecuador");
+        Equipo Venezuela = new Equipo("Venezuela");
+
+
     }
 }
