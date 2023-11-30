ae942a38f76dbd2164c21ed7777cb80c3c9270f9
Galoniax <axelian2504@gmail.com>
2023-11-30 15:37:10 -0300

    FEAT: Add Constructor and Getters and Setters to EtapaMundial class.

--- TP/src/Mundial/EtapaMundial.java
+++ TP/src/Mundial/EtapaMundial.java
@@ -6,4 +6,28 @@ public class EtapaMundial {
     private String name; // Nombre de etapa (Cuartos, Octavos, Final, Semi)
     private ArrayList<Equipo> equiposInvolucrados; // Lista de equipos involucrados en la etapa
 
+    public EtapaMundial(String name) {
+        setName(name);
+        equiposInvolucrados = new ArrayList<>();
+    }
+
+    public EtapaMundial() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Equipo> getEquiposInvolucrados() {
+        return equiposInvolucrados;
+    }
+
+    public void addEquipo(Equipo equipo) {
+        equiposInvolucrados.add(equipo);
+    }
 }
