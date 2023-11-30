6edfe377d904b218ad678e2048345bc2794376dd
Galoniax <axelian2504@gmail.com>
2023-11-30 15:34:18 -0300

    FEAT: Add Constructor and Getters and Setters to Equipo class.

--- TP/src/Mundial/Equipo.java
+++ TP/src/Mundial/Equipo.java
@@ -6,6 +6,42 @@ public class Equipo {
     private ArrayList<Partido> PartidosJugados;
     private ArrayList<Jugador> Jugadores;
 
+    //Constructor
+    public Equipo(String Name) {
+        this.Jugadores = new ArrayList<Jugador>();
+        this.PartidosJugados = new ArrayList<Partido>();
+        this.setName(Name);
 
+    }
+
+
+    //Getters
+    public String getName() {
+        return Name;
+    }
+
+    public ArrayList<Partido> getPartidosJugados() {
+        return PartidosJugados;
+    }
+
+
+    public ArrayList<Jugador> getJugadores() {
+        return Jugadores;
+    }
+
+
+
+    //Setters
+    public void setPartidosJugados(ArrayList<Partido> partidosJugados) {
+        PartidosJugados = partidosJugados;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        Jugadores = jugadores;
+    }
 
 }
