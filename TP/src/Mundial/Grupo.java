988d10bdf814bef920d166a6665c6af9a40c44bf
Galoniax <axelian2504@gmail.com>
2023-11-30 15:38:33 -0300

    FEAT: Add Constructor and Getters and Setters to Grupo class.

--- TP/src/Mundial/Grupo.java
+++ TP/src/Mundial/Grupo.java
@@ -6,4 +6,34 @@ public class Grupo extends EtapaMundial {
     private ArrayList<Equipo> equipos;
     private String nombreGrupo;
 
+    public Grupo(String nombreGrupo) {
+        super();
+        this.nombreGrupo = nombreGrupo;
+        this.equipos = new ArrayList<>();
+
+    }
+
+
+    public void addEquipos(Equipo equipo) {
+        equipos.add(equipo);
+    }
+
+
+    public ArrayList<Equipo> getEquiposConformados() {
+        return equipos;
+    }
+
+    public void setEquiposConformados(ArrayList<Equipo> equiposConformados) {
+        this.equipos = equiposConformados;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+
 }
