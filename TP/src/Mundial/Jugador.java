9c28534e9557286a3b16cbcc2ad4686be8973c94
Galoniax <axelian2504@gmail.com>
2023-11-30 15:41:48 -0300

    FEAT: Add Getters and Setters to Jugador class.

--- TP/src/Mundial/Jugador.java
+++ TP/src/Mundial/Jugador.java
@@ -7,4 +7,55 @@ public class Jugador {
     private int ContadorGoles;
     private Equipo Equipo;
 
+    public Jugador(String Nombre, int Numero, String Posicion) {
+        this.setNombre(Nombre);
+        this.setNumero(Numero);
+        this.setPosicion(Posicion);
+    }
+
+    //Getters
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public String getPosicion() {
+        return Posicion;
+    }
+
+    public int getContadorGoles() {
+        return ContadorGoles;
+    }
+
+    public Equipo getEquipo() {
+        return Equipo;
+    }
+
+
+    //Setters
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
+    public void setPosicion(String posicion) {
+        Posicion = posicion;
+    }
+
+    public void setContadorGoles(int contadorGoles) {
+        ContadorGoles = contadorGoles;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        Equipo = equipo;
+    }
+
 }
