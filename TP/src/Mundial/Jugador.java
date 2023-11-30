0fac705bb2eb20c773a07befa8a036cc0ff2a687
Galoniax <axelian2504@gmail.com>
2023-11-30 15:41:04 -0300

    FEAT: Add Variables Nombre, Numero, Posicion, ContadorGoles, Equipo to Jugador class.

--- TP/src/Mundial/Jugador.java
+++ TP/src/Mundial/Jugador.java
@@ -1,4 +1,10 @@
 package Mundial;
 
 public class Jugador {
+    private String Nombre;
+    private int Numero;
+    private String Posicion;
+    private int ContadorGoles;
+    private Equipo Equipo;
+
 }
