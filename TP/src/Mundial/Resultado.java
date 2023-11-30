fda09b3bb453c2a9d1baf1b0eb9ec86aea1ee6c1
Galoniax <axelian2504@gmail.com>
2023-11-30 15:51:17 -0300

    FEAT: Add Setters and Functions ganoLocal(), ganoVisitante(), empate() to Resultado class.

--- TP/src/Mundial/Resultado.java
+++ TP/src/Mundial/Resultado.java
@@ -4,6 +4,8 @@ public class Resultado {
     private int golesLocal;
     private int golesVisitante;
 
+
+    //Getters
     public int getGolesLocal() {
         return golesLocal;
     }
@@ -11,4 +13,35 @@ public class Resultado {
     public int getGolesVisitante() {
         return golesVisitante;
     }
+
+
+    //Setters
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+
+
+    //Functions
+    public boolean ganoLocal() {
+        return golesLocal > golesVisitante;
+    };
+
+    public boolean ganoVisitante() {
+        return golesLocal < golesVisitante;
+    };
+
+    public boolean empate() {
+        return golesVisitante == golesLocal;
+    }
+
+
+
+
+    //public void incrementVisitantGoals() {setGolesVisitante(getGolesVisitante() + 1);}
+
+    // public void incrementLocalGoals() {setGolesLocal(getGolesLocal() + 1);}
 }
