bfbec3dc0aad7c6911539cb5d44f3a3c833ece3d
Galoniax <axelian2504@gmail.com>
2023-11-30 15:33:36 -0300

    FEAT: Add Variables Name, PartidosJugados, Jugadores to Equipo class.

--- TP/src/Mundial/Equipo.java
+++ TP/src/Mundial/Equipo.java
@@ -1,4 +1,11 @@
 package Mundial;
+import java.util.ArrayList;
 
 public class Equipo {
+    private String Name;
+    private ArrayList<Partido> PartidosJugados;
+    private ArrayList<Jugador> Jugadores;
+
+
+
 }
