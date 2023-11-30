da25549720365c0beec9216dccd80c72c8dfd507
Galoniax <axelian2504@gmail.com>
2023-11-30 15:54:52 -0300

    FEAT: Create mostrarResultadoPartido() to class Main.

--- TP/src/Main.java
+++ TP/src/Main.java
@@ -2,6 +2,16 @@ import Mundial.*;
 import java.util.ArrayList;
 
 public class Main {
+    public static void mostrarResultadoPartido(Partido partido) {
+        if (partido.getResultado().ganoLocal()) {
+            System.out.println("Ganó " + partido.getLocal().getName());
+        } else if (partido.getResultado().ganoVisitante()) {
+            System.out.println("Ganó " + partido.getVisitante().getName());
+        } else {
+            System.out.println("Empate entre " + partido.getLocal().getName() + " y " + partido.getVisitante().getName());
+        }
+    }
+
     public static void main(String[] args) {
 
     }
