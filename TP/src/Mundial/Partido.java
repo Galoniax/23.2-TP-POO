7db3bcb5307bee19cd0c3b042886ee887865a679
Galoniax <axelian2504@gmail.com>
2023-11-30 15:48:41 -0300

    FEAT: Add Constructor and Getters to Partido class.

--- TP/src/Mundial/Partido.java
+++ TP/src/Mundial/Partido.java
@@ -8,4 +8,29 @@ public class Partido {
     private Equipo visitante;
     private Resultado resultado;
 
+    public Partido(Date fecha, Equipo local, Equipo visitante) {
+        this.fecha = fecha;
+        this.local = local;
+        this.visitante = visitante;
+        this.resultado = new Resultado(); // Inicializar el objeto resultado
+    }
+
+    public Equipo getLocal() {
+        return local;
+    }
+
+    public Equipo getVisitante() {
+        return visitante;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+
+    //public void incrementLocalGoals() {resultado.setGolesLocal(resultado.getGolesLocal() + 1);}
+
+    //public void incrementVisitantGoals() {resultado.setGolesVisitante(resultado.getGolesVisitante() + 1);}
+
+
 }
