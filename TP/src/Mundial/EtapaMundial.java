626b0e50417f6fe1c2f77956e564fbf41a7d2f50
Galoniax <axelian2504@gmail.com>
2023-11-30 15:36:30 -0300

    FEAT: Add Variables Name, EquiposInvolucrados to EtapaMundial class.

--- TP/src/Mundial/EtapaMundial.java
+++ TP/src/Mundial/EtapaMundial.java
@@ -1,4 +1,9 @@
 package Mundial;
 
+import java.util.ArrayList;
+
 public class EtapaMundial {
+    private String name; // Nombre de etapa (Cuartos, Octavos, Final, Semi)
+    private ArrayList<Equipo> equiposInvolucrados; // Lista de equipos involucrados en la etapa
+
 }
