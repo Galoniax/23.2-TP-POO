44f0f7b96dd27d771e2d0f4f00e4849d3643431a
Galoniax <axelian2504@gmail.com>
2023-11-30 15:49:42 -0300

    FEAT: Add Variables golesLocal, golesVisitante and Getters to Resultado class.

--- TP/src/Mundial/Resultado.java
+++ TP/src/Mundial/Resultado.java
@@ -1,4 +1,14 @@
 package Mundial;
 
 public class Resultado {
+    private int golesLocal;
+    private int golesVisitante;
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
 }
