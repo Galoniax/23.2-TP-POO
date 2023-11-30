d8d5411c6ced08c47f78d4f63eeb2dbd9066d571
Galoniax <axelian2504@gmail.com>
2023-11-30 15:37:58 -0300

    FEAT: Add Variables equipos, nombreGrupo to Grupo class.

--- TP/src/Mundial/Grupo.java
+++ TP/src/Mundial/Grupo.java
@@ -1,4 +1,9 @@
 package Mundial;
 
-public class Grupo {
+import java.util.ArrayList;
+
+public class Grupo extends EtapaMundial {
+    private ArrayList<Equipo> equipos;
+    private String nombreGrupo;
+
 }
